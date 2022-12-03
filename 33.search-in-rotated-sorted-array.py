d8c25d147ab7813800c3889565c3dbcb5837cab5
WOo0W <24245520+WOo0W@users.noreply.github.com>
2022-12-03 16:22:25 +0000

    33

---
+++ 33.search-in-rotated-sorted-array.py
@@ -0,0 +1,34 @@
+#
+# @lc app=leetcode id=33 lang=python3
+#
+# [33] Search in Rotated Sorted Array
+#
+
+
+# @lc code=start
+class Solution:
+
+    def search(self, nums: list[int], target: int) -> int:
+        return self.binary_search(nums, target, 0, len(nums) - 1)
+
+    def binary_search(self, nums, target, left, right):
+        if left > right:
+            return -1
+
+        mid = left + (right - left + 1) // 2
+        if nums[mid] == target:
+            return mid
+
+        if nums[mid] >= nums[left]:
+            if nums[left] <= target < nums[mid]:
+                return self.binary_search(nums, target, left, mid - 1)
+            else:
+                return self.binary_search(nums, target, mid + 1, right)
+        else:
+            if nums[mid] < target <= nums[right]:
+                return self.binary_search(nums, target, mid + 1, right)
+            else:
+                return self.binary_search(nums, target, left, mid - 1)
+
+
+# @lc code=end
